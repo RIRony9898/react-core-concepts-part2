8b1e5eb2202c29c74acc4304486ea1c5ae57025f
Md Raisul Islam Rony <rirony59@gmail.com>
2025-01-23 18:46:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  function handleClick() {
+    alert("Button clicked");
+  }
+  const handleClick2 = () => {
+    alert("Button 2 clicked");
+  };
+  const handleAddFive = (num) => {
+    alert("The output is: " + (num + 5));
+  };
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+      {/* With normal function */}
+      <button onClick={handleClick}>Click me</button>
+      {/* with arrow function */}
+      <button onClick={handleClick2}>Click me 2</button>
+      {/* with inline function */}
+      <button onClick={() => {alert("Third button clicked");}}>Click me 3</button>
+      {/* event handler with params */}
+      <button onClick={() => handleAddFive(3)}>Add 5</button>
+    </>
+  );
+}
+
+export default App;
