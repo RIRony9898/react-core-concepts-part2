7aea44b216163a99f85549476e3371fdad1c544a
Md Raisul Islam Rony <rirony59@gmail.com>
2025-01-23 19:06:27 +0600

    react core concept (state management & event handling) revised practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Counter from "./Counter";
 
 function App() {
   function handleClick() {
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <h3>React Core Concepts 2</h3>
+      <Counter/>
       {/* With normal function */}
       <button onClick={handleClick}>Click me</button>
       {/* with arrow function */}
